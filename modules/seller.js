8e7c23402122f736a64e6400c5d4d1ca5c82221a
Akshay Jangir <52463851+axy1976@users.noreply.github.com>
2021-12-04 23:34:27 +0530

    Update seller.js

--- modules/seller.js
+++ modules/seller.js
@@ -1,26 +1,15 @@
-[
-    {
-        "sellerid":"s1",
-        "name":"Jhon",
-        "productids":[
-            "p1",
-            "p3"
-        ]
-    },
-    {
-        "sellerid":"s2",
-        "name":"Action",
-        "productids":[
-            "p2",
-            "p5",
-            "p6"
-        ]
-    },
-    {
-        "sellerid":"s3",
-        "name":"Russel",
-        "productids":[
-            "p4"
-        ]
-    }
-]
+
+const mongoose = require("mongoose");
+mongoose.pluralize(null);
+
+const sellerSchema = mongoose.Schema( {
+    sellerid : String,
+    name : String,
+    productids : [{
+        type:String
+    }]
+});
+
+const sellerModel = mongoose.model("Seller",sellerSchema);
+
+module.exports= sellerModel;
